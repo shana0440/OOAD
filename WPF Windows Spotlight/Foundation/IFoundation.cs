0b3a47a610b338deb1de0e4d9754247c7cb1da52
kw <shana0440@gmail.com>
2016-04-24 15:13:19 +0800

    dowork來自system componentModel

--- WPF Windows Spotlight/Foundation/IFoundation.cs
+++ WPF Windows Spotlight/Foundation/IFoundation.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace WPF_Windows_Spotlight.Foundation
 {
     public interface IFoundation
     {
-        void DoWork();
+        void DoWork(object sender, DoWorkEventArgs e);
     }
 }
