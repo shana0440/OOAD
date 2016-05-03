b33071fd982a5eea2a0b3244b3a8112ffbc9a5af
kw <shana0440@gmail.com>
2016-05-03 19:33:24 +0800

    use backgroundworker call foundation, and add adapterTest

--- WPF Windows Spotlight/Adapter.cs
+++ WPF Windows Spotlight/Adapter.cs
@@ -12,20 +12,42 @@ namespace WPF_Windows_Spotlight
     public class Adapter
     {
         private string _keyword;
+        private List<BackgroundWorker> _workers;
+        private string _result;
 
         public Adapter()
         {
-
+            _workers = new List<BackgroundWorker>();
         }
 
         public void Search(string keyword)
         {
-            
+            CancelBackgroundWorker();
+
+            BackgroundWorker bgworker = new BackgroundWorker();
+            IFoundation foundation = new Calculator(keyword);
+            bgworker.RunWorkerCompleted += WorkerCompleted;
+            bgworker.DoWork += foundation.DoWork;
+            bgworker.RunWorkerAsync();
+        }
+
+        public string GetResult()
+        {
+            return _result;
+        }
+
+        private void CancelBackgroundWorker()
+        {
+            foreach (BackgroundWorker worker in _workers)
+            {
+                worker.CancelAsync();
+            }
         }
 
         private void WorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
             Console.WriteLine("Result = " + e.Result);
+            _result = e.Result.ToString();
         }
     }
 }
