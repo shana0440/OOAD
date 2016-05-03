c8b9583112d5f05228d5da97a9584495151bccf7
kw <shana0440@gmail.com>
2016-05-03 20:07:46 +0800

    add foundation factory

---
+++ WPF Windows Spotlight/Foundation/FoundationFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Windows_Spotlight.Foundation
+{
+    public class FoundationFactory
+    {
+        public IFoundation CreateFoundation(string foundationName, string arg)
+        {
+            IFoundation foundation;
+            switch (foundationName)
+	        {
+                case "calculator":
+                    foundation = new Calculator(arg);
+                    break;
+		        default:
+                    foundation = null;
+                    break;
+	        }
+            return foundation;
+        }
+    }
+}
