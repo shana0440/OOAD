65f567e49a02da295adf9f93a52a38f4e3147fe3
kw <shana0440@gmail.com>
2016-05-28 17:05:20 +0800

    匯率轉換

--- WPF Windows Spotlight/Foundation/Exchange.cs
+++ WPF Windows Spotlight/Foundation/Exchange.cs
@@ -7,7 +7,9 @@ using HtmlAgilityPack;
 using System.Net;
 using System.ComponentModel;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Web;
+using WPF_Windows_Spotlight.Foundation.ItemType;
 
 namespace WPF_Windows_Spotlight.Foundation
 {
@@ -22,12 +24,18 @@ namespace WPF_Windows_Spotlight.Foundation
             _currency = currency;
         }
 
-        public string Currency
+        public void SetKeyword(string keyword)
         {
-            set { _currency = value; }
+            _currency = keyword;
         }
-
-        private HtmlNode GetTable()
+        
+        /**
+         * DOTO 用正規表達式將currency分成數字以及幣別兩部分
+         * 透過GetTable取得臺灣銀行公告的幣別匯率
+         * 遊歷所有幣別直到找到使用者輸入的幣別
+         * 將匯率換算成台幣
+         */
+        public string ExchangeCurrency(string currency)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(_url);
             request.Accept = "text/html";
@@ -39,34 +47,62 @@ namespace WPF_Windows_Spotlight.Foundation
                 string html = stream.ReadToEnd();
                 HtmlDocument dom = new HtmlDocument();
                 dom.LoadHtml(html);
-                HtmlNode node = dom.DocumentNode.SelectSingleNode(_xpath);
-                return node;
-            }
-        }
-
-        /**
-         * DOTO 用正規表達式將currency分成數字以及幣別兩部分
-         * 透過GetTable取得臺灣銀行公告的幣別匯率
-         * 遊歷所有幣別直到找到使用者輸入的幣別
-         * 將匯率換算成台幣
-         */
-        private string ExchangeCurrency(string currency)
-        {
-            HtmlNode table = GetTable();
-            for (int i = 5; i < table.ChildNodes.Count; i+=2)
-            {
-                HtmlNode row = table.ChildNodes[i];
+                var rows = GetExchangeRows(dom);
+                var convertCurrency = currency.Substring(currency.Length - 3, 3);
+                var value = currency.Substring(0, currency.Length - 3);
+                foreach (var row in rows)
+                {
+                    var currencyName = GetCurrencyName(row);
+                    var sell = GetSell(row);
+                    var buy = GetBuy(row);
+                    if (convertCurrency == currencyName)
+                    {
+                        var result = Double.Parse(value)*Double.Parse(sell);
+                        return result.ToString();
+                    }
+                }
             }
             return "";
         }
 
-        public string GetResult()
+        private string GetBuy(HtmlNode row)
         {
-            return ExchangeCurrency(_currency);
+            var buy = row.ChildNodes[1].InnerText;
+            return buy;
+        }
+
+        private string GetSell(HtmlNode row)
+        {
+            var sell = row.ChildNodes[2].InnerText;
+            return sell;
+        }
+
+        private string GetCurrencyName(HtmlNode row)
+        {
+            var currency = row.FirstChild.InnerText;
+            string pattern = @"\((\w+)\)";
+            Match m = Regex.Match(currency, pattern);
+            return m.Value.Substring(1, m.Value.Length - 2);
+        }
+
+        private HtmlNodeCollection GetExchangeRows(HtmlDocument dom)
+        {
+            var xpath = "//tr[contains(@class, 'color')]";
+            var nodes = dom.DocumentNode.SelectNodes(xpath);
+            return nodes;
         }
 
         public void DoWork(object sender, DoWorkEventArgs e)
         {
+            var list = new List<Item>();
+            var result = ExchangeCurrency(_currency);
+            var bg = sender as BackgroundWorker;
+            if (bg.CancellationPending)
+            {
+                e.Cancel = true;
+                return;
+            }
+            e.Result = new KeyValuePair<string, List<Item>>((string)e.Argument, list);
 
         }
     }
