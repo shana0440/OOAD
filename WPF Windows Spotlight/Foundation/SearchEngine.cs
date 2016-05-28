557dbc86d0a09a003371dfaa2492b843f129170a
kw <shana0440@gmail.com>
2016-05-28 15:32:06 +0800

    搜尋功能完成

---
+++ WPF Windows Spotlight/Foundation/SearchEngine.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using WPF_Windows_Spotlight.Foundation.ItemType;
+
+namespace WPF_Windows_Spotlight.Foundation
+{
+    public class SearchEngine : IFoundation
+    {
+        private string _keyword;
+        private readonly string _url;
+        private readonly Bitmap _icon;
+
+        public SearchEngine()
+        {
+            _url = "https://www.google.com.tw/search?q=";
+            _icon = (Bitmap)WPF_Windows_Spotlight.Properties.Resources.web;
+        }
+
+        public List<Item> Search()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(_url + _keyword);
+            request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            request.Method = "GET";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
+            request.Headers.Set("accept-language", "zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4");
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader stream = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                string html = stream.ReadToEnd();
+                HtmlDocument dom = new HtmlDocument();
+                dom.LoadHtml(html);
+                return GetWebSite(dom, 5);
+            }
+
+            return null;
+        }
+
+        private List<Item> GetWebSite(HtmlDocument dom, int count)
+        {
+            var result = new List<Item>();
+
+            var titles = GetTitles(dom);
+            if (titles == null)
+                return result;
+            var intros = GetIntros(dom);
+            var urls = GetUrls(dom);
+
+            for (int i = 0; i < count; i++)
+            {
+                var title = WebUtility.HtmlDecode(titles[i].InnerText);
+                var intro = WebUtility.HtmlDecode(intros[i].InnerText);
+                var url = WebUtility.HtmlDecode(urls[i].InnerText);
+                var web = new WebSite(title, intro, url);
+                web.SetIcon(_icon);
+                result.Add(web);
+            }
+            return result;
+        }
+
+        private HtmlNodeCollection GetUrls(HtmlDocument dom)
+        {
+            var xpath = "//div[contains(@class, 'kv')]/cite";
+            var nodes = dom.DocumentNode.SelectNodes(xpath);
+            return nodes;
+        }
+
+        private HtmlNodeCollection GetIntros(HtmlDocument dom)
+        {
+            var xpath = "//span[@class='st']";
+            var nodes = dom.DocumentNode.SelectNodes(xpath);
+            return nodes;
+        }
+
+        private HtmlNodeCollection GetTitles(HtmlDocument dom)
+        {
+            var xpath = "//div[@class='s']/preceding-sibling::h3[@class='r']";
+            var nodes = dom.DocumentNode.SelectNodes(xpath);
+            return nodes;
+        }
+        
+        public void SetKeyword(string keyword)
+        {
+            _keyword = keyword;
+        }
+
+        public void DoWork(object sender, DoWorkEventArgs e)
+        {
+            var results = Search();
+            var bg = sender as BackgroundWorker;
+            if (bg.CancellationPending)
+            {
+                e.Cancel = true;
+                return;
+            }
+            e.Result = new KeyValuePair<string, List<Item>>((string)e.Argument, results);
+
+        }
+
+    }
+}
---
+++ Windows Spotlight Test/SearchEngineTester.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WPF_Windows_Spotlight.Foundation;
+using WPF_Windows_Spotlight.Foundation.ItemType;
+
+namespace Windows_Spotlight_Test
+{
+    [TestClass]
+    public class SearchEngineTester
+    {
+        private SearchEngine _system;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _system = new SearchEngine();
+        }
+
+        [TestMethod]
+        public void TestSearch()
+        {
+            var expected = new List<WebSite>();
+            expected.Add(new WebSite("How people build software · GitHub", "Online project hosting using Git. Includes source-code browser, in-line editing, wikis, and ticketing. Free for public open-source code. Commercial closed source ...", "https://github.com/"));
+            _system.SetKeyword("github");
+            var results = _system.Search();
+            for (int i = 0; i < expected.Count; i++)
+            {
+                var result = (WebSite) results[i];
+                Assert.AreEqual(expected[i].Title, result.Title);
+                Assert.AreEqual(expected[i].Intro, result.Intro);
+                Assert.AreEqual(expected[i].Url, result.Url);
+            }
+        }
+
+        [TestMethod]
+        public void TestSearchNoResult()
+        {
+            _system.SetKeyword("qowiejalskdfjqlwkehdskljfhqaksjdslkd");
+            var results = _system.Search();
+            Assert.AreEqual(0, results.Count);
+        }
+    }
+}
