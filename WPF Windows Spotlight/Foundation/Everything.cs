dd2f9b5f2f68e0a1ff498562e8082872850b53d1
kw <shana0440@gmail.com>
2016-06-15 16:29:06 +0800

    環境趕成x64

--- WPF Windows Spotlight/Foundation/Everything.cs
+++ WPF Windows Spotlight/Foundation/Everything.cs
@@ -15,65 +15,65 @@ namespace WPF_Windows_Spotlight.Foundation
         const int EVERYTHING_ERROR_INVALIDINDEX = 6;
         const int EVERYTHING_ERROR_INVALIDCALL = 7;
 
-        [DllImport("Everything32.dll", CharSet = CharSet.Unicode)]
+        [DllImport("Everything64.dll", CharSet = CharSet.Unicode)]
         public static extern int Everything_SetSearchW(string lpSearchString);
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern void Everything_SetMatchPath(bool bEnable);
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern void Everything_SetMatchCase(bool bEnable);
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern void Everything_SetMatchWholeWord(bool bEnable);
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern void Everything_SetRegex(bool bEnable);
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern void Everything_SetMax(int dwMax);
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern void Everything_SetOffset(int dwOffset);
 
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern bool Everything_GetMatchPath();
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern bool Everything_GetMatchCase();
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern bool Everything_GetMatchWholeWord();
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern bool Everything_GetRegex();
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern UInt32 Everything_GetMax();
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern UInt32 Everything_GetOffset();
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern string Everything_GetSearchW();
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern int Everything_GetLastError();
 
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern bool Everything_QueryW(bool bWait);
 
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern void Everything_SortResultsByPath();
 
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern int Everything_GetNumFileResults();
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern int Everything_GetNumFolderResults();
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern int Everything_GetNumResults();
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern int Everything_GetTotFileResults();
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern int Everything_GetTotFolderResults();
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern int Everything_GetTotResults();
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern bool Everything_IsVolumeResult(int nIndex);
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern bool Everything_IsFolderResult(int nIndex);
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern bool Everything_IsFileResult(int nIndex);
-        [DllImport("Everything32.dll", CharSet = CharSet.Unicode)]
+        [DllImport("Everything64.dll", CharSet = CharSet.Unicode)]
         public static extern void Everything_GetResultFullPathNameW(int nIndex, StringBuilder lpString, int nMaxCount);
-        [DllImport("Everything32.dll")]
+        [DllImport("Everything64.dll")]
         public static extern void Everything_Reset();
     }
 }
