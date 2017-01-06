537dc52a542f5e447525f47fe39c951205dd8aab
kw <shana0440@gmail.com>
2017-01-07 02:48:20 +0800

    1. change view position 2. block search engine

--- WPF Windows Spotlight/Foundation/FoundationFactory.cs
+++ WPF Windows Spotlight/Foundation/FoundationFactory.cs
@@ -15,14 +15,14 @@ namespace WPF_Windows_Spotlight.Foundation
             _order.Add("匯率換算", new List<Item>());
             _order.Add("翻譯", new List<Item>());
             _order.Add("網頁", new List<Item>());
-            _order.Add("檔案或資料夾", new List<Item>());
+            //_order.Add("檔案或資料夾", new List<Item>());
 
             var keys = _order.Keys.ToArray();
 
             _foundations.Add(new Calculator(keys[0]));
             _foundations.Add(new Exchange(keys[1]));
             _foundations.Add(new Translator(keys[2]));
-            _foundations.Add(new SearchEngine(keys[3]));
+            //_foundations.Add(new SearchEngine(keys[3]));
             _foundations.Add(new FileSystem(keys[4]));
         }
 
